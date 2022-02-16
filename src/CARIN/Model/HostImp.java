5ae8c95f427bb366ffdd9c74f60ae7a5b598100a
lalinthorn <lalinpye@gmail.com>
2022-02-16 20:16:55 +0700

    fix bug getNearBy1

--- src/CARIN/Model/HostImp.java
+++ src/CARIN/Model/HostImp.java
@@ -221,58 +221,62 @@ public class HostImp implements Host{
     @Override
     public int getNearBy(String direction) {
         String dir = direction.toLowerCase();
+        List<Host>host = new LinkedList<>();
         int ans = m*n*10;
+        for (Host h : body.getOrganism()) {
+           if(h != this) host.add(h);
+        }
         if(dir.equals("up") && location[0]>1){
-            for (Host h : body.getOrganism()) {
+            for (Host h : host) {
                 if (h.getLocation()[1] == location[1] && h.getLocation()[0]<location[0] ) {
                     int a = ((location[0] - h.getLocation()[0])*10) + h.getType();
                     if(a<ans && a>0) ans = a;
                 }
             }
         }else if (dir.equals("down") && location[0]<m){
-            for (Host h : body.getOrganism()) {
+            for (Host h : host) {
                 if (h.getLocation()[1] == location[1] && h.getLocation()[0]>location[0] ) {
                     int a =((h.getLocation()[0] - location[0])*10)+h.getType();
                     if(a<ans && a>0) ans = a;
                 }
             }
         }else if (dir.equals("left") && location[1]>0){
-            for (Host h : body.getOrganism()) {
+            for (Host h : host) {
                 if (h.getLocation()[0] == location[0] && h.getLocation()[1]<location[1] ) {
                     int a = ((location[1] - h.getLocation()[1])*10)+h.getType();
                     if(a<ans && a>0) ans = a;
                 }
             }
         }else if (dir.equals("right") && location[1]<n){
-            for (Host h : body.getOrganism()) {
+            for (Host h : host) {
                 if (h.getLocation()[0] == location[0] && h.getLocation()[1]>location[1] ) {
                     int a =((h.getLocation()[1] - location[1])*10)+h.getType();
                     if(a<ans && a>0) ans = a;
                 }
             }
         }else if (dir.equals("upleft") && location[0]>1 && location[1]>0){
-            for (Host h : body.getOrganism()) {
+            for (Host h : host) {
                 if (location[0] - h.getLocation()[0] == location[1] - h.getLocation()[1]) {
                     int a = ((location[0] - h.getLocation()[0])*10)+h.getType();
                     if(a<ans && a>0) ans = a;
                 }
             }
         }else if(dir.equals("upright") && location[0]>1 && location[1]<n){
-            for (Host h : body.getOrganism()) {
+            for (Host h : host) {
                 if (location[0]-h.getLocation()[0] == h.getLocation()[1]-location[1] ) {
                     int a = ((location[0] - h.getLocation()[0])*10)+h.getType();
                     if(a<ans && a>0) ans = a;
                 }
             }
         }else if (dir.equals("downleft") && location[0]<m && location[1]>0){
-            for (Host h : body.getOrganism()) {
+            for (Host h : host) {
                 if (h.getLocation()[0] - location[0] == location[1] - h.getLocation()[1]) {
                     int a = ((h.getLocation()[0] - location[0])*10)+h.getType();
                     if(a<ans && a>0) ans = a;
                 }
             }
         }else if (dir.equals("downright") && location[0]<m && location[1]<n){
-            for (Host h : body.getOrganism()) {
+            for (Host h : host) {
                 if (h.getLocation()[0] - location[0] == h.getLocation()[1] - location[1]) {
                     int a = ((h.getLocation()[0] - location[0])*10)+h.getType();
                     if(a<ans && a>0) ans = a;
