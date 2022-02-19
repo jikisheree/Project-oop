cf63e7d752cd3e0eb15037692077520c2d994356
Suparida <jik2002x@gmail.com>
2022-02-19 14:09:21 +0700

    make private final + throws errors of eval classes

--- src/CARIN/Parser/Command.java
+++ src/CARIN/Parser/Command.java
@@ -7,34 +7,45 @@ import java.util.Map;
 
 public class Command implements Program {
 
-    String type;
-    String direction;
-    Expr expression;
-    Host host;
-    Map<String, Expr> identifier;
+    private final String type;
+    private final String direction;
+    private final Expr expression;
+    private final Host host;
+    private final Map<String, Expr> identifier;
 
     public Command(String type, String direction, Host host){
         this.type = type;
         this.direction = direction;
         this.host = host;
+        expression = null;
+        identifier = null;
     }
     public Command(String type, Expr expr, HashMap<String, Expr> identifier){
         this.type = type;
         this.expression = expr;
         this.identifier = identifier;
+        direction = null;
+        host = null;
     }
 
     @Override
     public void eval() {
-        if(type.equals("move")){
-            host.move(direction);
+        try {
+            if (type.equals("move")) {
+                assert host != null;
+                host.move(direction);
 //            System.out.println("move"+direction);
-        }else if(type.equals("shoot")){
-            host.shoot(direction);
+            } else if (type.equals("shoot")) {
+                assert host != null;
+                host.shoot(direction);
 //            System.out.println("shoot"+direction);
-        }else{
-            // assignment statement
-            identifier.put(type, expression);
+            } else {
+                // assignment statement
+                assert identifier != null;
+                identifier.put(type, expression);
+            }
+        }catch (EvalError e) {
+            throw new EvalError("Cannot evaluate "+type);
         }
     }
 }
